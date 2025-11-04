792694791af410d2bc8a67d3220a6abd61271513
RubabaBinteMahbub <rubababintemahbub2007@gmail.com>
2025-11-04 20:37:47 +0600

    Add program to check divisibility by 3 or 5

---
+++ divisible.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int a;
+printf("Enter the value of a : \n");
+scanf("%d", &a);
+
+if(a%3 == 0 || a%5 == 0){
+printf("The number is divisible by 3      or 5.\n");
+}
+else{
+printf("The number is not divisible by 3 or 5.\n");
+}
+
+return 0;
+}
